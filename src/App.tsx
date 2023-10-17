a91ff67d63a523740e9c163449d09fd81de60cd8
RITESH MANE <maneritesh@gmail.com>
2023-10-17 10:30:22 +0530

    routing setup 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <Routes>
+      <Route path='/' element={<div>Landing page</div>} />
+      <Route path='/signin' element={<div>hello</div>} />
+      <Route path='/login' element={<div>hello</div>} />
+      <Route path='/cart' element={<div>my cart</div>} />
+      <Route path='/products' element={<div>all Product</div>} />
+      <Route path='/products/:id' element={<div>details of one products</div>} />
+      <Route path='/waredrobe' element={<div>people suggest the new pairing option waredrobes</div>} />
+      <Route path='*' element={<div>404 page not found got to home page</div>} />
+    </Routes>
   );
 }
 
