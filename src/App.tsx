21ae8f9a0d3e93a7281622acae158960229fdc81
RITESH MANE <maneritesh@gmail.com>
2023-10-19 20:58:13 +0530

    added sign in page

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,13 @@ import Errorpage from "./pages/ErrorPage/Error";
 import LandingPage from "./pages/LandingPage";
 import Login from "./pages/authpages/Login";
 import HomeLayout from "./components/HomeLayout";
+import SignIn from "./pages/authpages/SignIn";
 
 function App() {
   return (
     <Routes>
       <Route path="/login" element={<Login />} />
-      <Route path="/signin" element={<div>hello</div>} />
+      <Route path="/signin" element={<SignIn />} />
 
       <Route path="/" element={<HomeLayout />}>
         <Route index element={<LandingPage />} />
--- src/pages/authpages/SignIn.tsx
+++ src/pages/authpages/SignIn.tsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import { Link } from "react-router-dom";
 
 const SignIn = () => {
   return (
-    <div>SignIn</div>
-  )
-}
+    <div className="h-screen bg-slate-50">
+      <div className="h-[50vh] flex justify-center items-center relative bg-orange-100">
+        <div className="p-10 w-1/3 absolute top-32 z-20 bg-white flex flex-col border">
+          <h1 className="font-semibold text-lg mb-10">Create your new Account</h1>
+          <form className="flex flex-col mb-5 gap-5" action="">
+            <div className="flex gap-2 flex-col">
+              <label htmlFor="">Name</label>
+              <input className="p-2 outline-none bg-slate-50" type="text" />
+            </div>
+            <div className="flex gap-2 flex-col">
+              <label htmlFor="">Email</label>
+              <input className="p-2 outline-none bg-slate-50" type="text" />
+            </div>
+            <div className="flex gap-2 flex-col">
+              <label htmlFor="">Password</label>
+              <input className="p-2 outline-none bg-slate-50" type="password" />
+            </div>
+            <input
+              className="bg-orange-100 font-medium p-2 w-full"
+              type="submit"
+            />
+          </form>
+          <p className="text-[0.9rem] mt-10 text-center">
+            existing user ?{" "}
+            <Link to={"/login"} className="text-blue-500 underline">
+              login
+            </Link>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default SignIn
+export default SignIn;
